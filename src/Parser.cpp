ad2ab23c6ca67bc9ac43c842e4a30073f94d9a59
Matt Hellige <mhellige@drwholdings.com>
2011-08-31 17:42:17 -0500

    Added stuff to parser.

--- src/Parser.cpp
+++ src/Parser.cpp
@@ -6,36 +6,21 @@
 #include "Node.h"
 
 namespace qi = boost::spirit::qi;
+namespace phoenix = boost::phoenix;
 
 namespace ccs {
 
 namespace {
 
 template <typename Iterator>
-struct skip_grammar : qi::grammar<Iterator> {
-  qi::rule<Iterator> blockComment;
-  qi::rule<Iterator> start;
-
-  skip_grammar() : skip_grammar::base_type(start) {
-    using qi::char_;
-    using qi::space;
-    using qi::eol;
-    using qi::eoi;
-
-    blockComment = "/*" >> *(blockComment | (char_ - "*/")) >> "*/";
-    start = blockComment
-          | "//" >> *(char_ - eol) >> (eol | eoi)
-          | space;
-  }
-};
-
-template <typename Iterator>
-struct ccs_grammar : qi::grammar<Iterator, skip_grammar<Iterator>> {
-  typedef qi::rule<Iterator, skip_grammar<Iterator> > spacerule;
+struct ccs_grammar : qi::grammar<Iterator, qi::rule<Iterator>> {
+  typedef qi::rule<Iterator, qi::rule<Iterator>> spacerule;
   typedef qi::rule<Iterator> nospacerule;
 
+  nospacerule blockComment;
+  nospacerule skipper;
+
   nospacerule ident;
-  spacerule directive;
   spacerule property;
   spacerule selector;
   nospacerule step;
@@ -44,26 +29,46 @@ struct ccs_grammar : qi::grammar<Iterator, skip_grammar<Iterator>> {
   spacerule file;
 
   ccs_grammar() : ccs_grammar::base_type(file) {
-    using qi::eol;
     using qi::lit;
     using qi::char_;
+    using qi::space;
+    using qi::eol;
+    using qi::eoi;
+
+    // comments and whitespace...
+    blockComment = "/*" >> *(blockComment | (char_ - "*/")) >> "*/";
+    skipper = blockComment
+            | "//" >> *(char_ - eol) >> (eol | eoi)
+            | space;
 
     auto string = qi::lexeme['\'' >> *(char_ - ('\'' | eol)) >> '\''];
     auto val = string | qi::long_long | qi::double_ | lit("0x") >> qi::hex;
     ident = +char_("A-Za-z0-9$_") | string;
-    directive = lit("@import") >> string >> -lit(';');
+    auto context = lit("@context") >> -char_(">+") >> '(' >> selector >> ')'
+        >> -lit(';');
+    auto import = lit("@import") >> string >> -lit(';');
     property = ident >> '=' >> val >> -lit(';');
+    // TODO make '+' and ',' lower precedence than '>' and ' '...
+    // TODO allow operator at start of selector...
     selector = step >> '>' >> selector
+             | step >> '+' >> selector
              | step >> selector
              | step;
     step = ('*' | ident) >> -stepsuffix
-         | stepsuffix;
-    auto pseudo = lit("root");
+         | stepsuffix
+         | '(' >> qi::skip(skipper.alias())[selector] >> ')';
+    auto pseudo = lit("root") >> !ident;
     stepsuffix = '.' >> ident >> -stepsuffix
                | ':' >> pseudo >> -stepsuffix
-               | '#' >> ident >> -stepsuffix;
-    rule = property | (selector >> '{' >> +property >> '}' >> -lit(';'));
-    file = *(rule | directive);
+               | '#' >> ident >> -stepsuffix
+               | '[' >> qi::skip(skipper.alias())[ident >> '=' >> val >> ']' ]
+                                                  >> -stepsuffix;
+    rule = import | property | (selector >> '{' >> +rule >> '}' >> -lit(';'));
+    file = *context >> *rule;
+  }
+
+  bool parse(Iterator &iter, Iterator end) {
+    return qi::phrase_parse(iter, end, *this, skipper);
   }
 };
 
@@ -72,9 +77,8 @@ struct ccs_grammar : qi::grammar<Iterator, skip_grammar<Iterator>> {
 
 bool Parser::parseString(std::string input) {
   auto iter = input.begin();
-  skip_grammar<typeof(iter)> skip;
   ccs_grammar<typeof(iter)> grammar;
-  bool r = qi::phrase_parse(iter, input.end(), grammar, skip);
+  bool r = grammar.parse(iter, input.end());
 
   if (r && iter == input.end()) {
     std::cout << "Parsing succeeded\n";
@@ -90,9 +94,8 @@ bool Parser::parseString(std::string input) {
 
 bool Parser::parseString(Node &root, std::string input) {
   auto iter = input.begin();
-  skip_grammar<typeof(iter)> skip;
   ccs_grammar<typeof(iter)> grammar;
-  bool r = qi::phrase_parse(iter, input.end(), grammar, skip);
+  bool r = grammar.parse(iter, input.end());
 
   if (r && iter == input.end()) {
     std::cout << "Parsing succeeded\n";
--- test/ParserTest.cpp
+++ test/ParserTest.cpp
@@ -10,6 +10,8 @@ TEST(ParserTest, BasicPhrases) {
   Parser parser;
   EXPECT_TRUE(parser.parseString(""));
   EXPECT_TRUE(parser.parseString("@import 'file'"));
+  EXPECT_TRUE(parser.parseString("@context (foo :root > baz)"));
+  EXPECT_TRUE(parser.parseString("@context + (foo .bar > baz)"));
   EXPECT_TRUE(parser.parseString("prop = 'val'"));
   EXPECT_TRUE(parser.parseString("elem#id {prop = 'val'}"));
   EXPECT_TRUE(parser.parseString(".class.class :root > elem#id {prop=43}"));
@@ -17,8 +19,16 @@ TEST(ParserTest, BasicPhrases) {
   EXPECT_FALSE(parser.parseString(".class. class :root > elem#id {prop=\"val\"}"));
   EXPECT_FALSE(parser.parseString(".class.class : root > elem#id {prop=0xab12}"));
   EXPECT_FALSE(parser.parseString("blah"));
+  EXPECT_FALSE(parser.parseString("@import 'file'; @context (foo)"));
+  EXPECT_TRUE(parser.parseString(".class { @import 'file' }"));
+  EXPECT_FALSE(parser.parseString(".class { @context + (foo) }"));
   EXPECT_TRUE(parser.parseString("elem#id { prop = 'val'; prop2 = 31337 }"));
   EXPECT_TRUE(parser.parseString("*:root > * foo *.blah { p = 1; }"));
+  EXPECT_TRUE(parser.parseString("[prop='val'].foo[ p = 'hmm'] { p = 1; }"));
+  EXPECT_TRUE(parser.parseString("a b + c d {p=1}"));
+  EXPECT_TRUE(parser.parseString("(a b) + (c d) {p=1}"));
+  EXPECT_FALSE(parser.parseString(":rootfoo {p = 1}"));
+  EXPECT_TRUE(parser.parseString(":root foo {p = 1}"));
 }
 
 TEST(ParserTest, Comments) {
